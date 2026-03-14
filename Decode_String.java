ef50c66ce2ad9cd6b55cadf492045e204c5fed3d
happydahlia <106787929+happydahlia@users.noreply.github.com>
2026-03-13 23:04:14 -0500

    Decode String code

---
+++ Decode_String.java
@@ -0,0 +1,47 @@
+import java.util.Stack;
+
+public class Decode_String {
+    public String decode(String s){
+        Stack<Integer> numStack = new Stack<>();
+        Stack<String> strStack = new Stack<>();
+        String finalStr = "";
+        int k = 0;
+        for (int i = 0; i < s.length(); i++) {
+
+            char c = s.charAt(i);
+
+            if (Character.isDigit(c)) {
+                k = k * 10 + (c - '0');
+            }
+
+            else if (c == '[') {
+                numStack.push(k);
+                strStack.push(finalStr);
+                finalStr = "";
+                k = 0;
+            }
+
+            else if (c == ']') {
+                int repeat = numStack.pop();
+                String prev = strStack.pop();
+                String temp = "";
+                for (int j = 0; j < repeat; j++) {
+                    temp += finalStr;
+                }
+
+                finalStr = prev + temp;
+            }
+
+            else {
+                finalStr += c;
+            }
+        }
+
+        return finalStr;
+    }
+    public static void main(String[] args) {
+        Decode_String sequence = new Decode_String();
+        String Test = "2[abd2[ce3[rt]]]";
+        System.out.print(sequence.decode(Test));
+    }
+}
